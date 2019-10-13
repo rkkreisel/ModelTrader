085abeef228d242a5ce716bbfa4434801d9a9733
rkkreisel <randy.kreisel@gmail.com>
2019-10-13 13:30:51 -0400

    sync

--- logic.py
+++ logic.py
@@ -50,9 +50,12 @@ class Algo():
             #
             #start of study
             #
-            bars_15m = calculations.Calculations(dataContract,"2 D","15 mins",datetime_15m as datetime_period)
-            bars_1h = calculations.Calculations(dataContract,"5 D","1 hour",datetime_1h as datetime_period)
-            bars_1d = calculations.Calculations(dataContract,"75 D","1 day",datetime_1d as datetime_period)
+            timeperiod = "15"
+            bars_15m = calculations.Calculations()
+            timeperiod = "1h"
+            bars_1h = calculations.Calculations()
+            timeperiod = "1d"
+            bars_1d = calculations.Calculations()
             setsum = self.setupsummary(key_arr)
             log.info("tradenow: {trade}".format(trade = tradenow))
             #
