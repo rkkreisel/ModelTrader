7753e24a3d8222321570746828ba9f175019ab8c
rkkreisel <randy.kreisel@gmail.com>
2020-01-08 08:55:31 -0500

    sync

--- logic.py
+++ logic.py
@@ -49,7 +49,7 @@ class Algo():
             dataContract = Contract(exchange=config.EXCHANGE, secType="FUT", localSymbol=contContract.localSymbol)
             log.debug("Got Contract: {}".format(dataContract.localSymbol))
             self.app.contract.update(dataContract.localSymbol)
-            wait_time,self.datetime_15,self.datetime_1h,self.datetime_1d, self.log_time = self.define_times()
+            wait_time,self.datetime_15,self.datetime_1h,self.datetime_1d, self.log_time = self.define_times(self.ib)
             log.debug("next datetime for 15 minutes - should be 15 minutes ahead of desired nextqtr{}".format(wait_time))
             #
             # debug 
@@ -57,6 +57,7 @@ class Algo():
             #wait_time = wait_time = current_time.replace(minute = 1,second=0)
             #
             self.ib.waitUntil(wait_time)
+            
             log.info("before loop start:{ls}".format(ls=datetime.now()))
             self.ib.loopUntil(condition=self.ib.isConnected())   # rying to fix 1100 error on nightly reset
             log.info("after loop start:{ls}".format(ls=datetime.now()))
@@ -120,32 +121,61 @@ class Algo():
             wrote_bar_to_csv = helpers.build_csv_bars_row(self.log_time, tradeAction, bars_15m, bars_1h, bars_1d, pendingLong, pendingShort, pendingCnt, tradeNow, ccibb_trade, cci_trade,ccibb_key, cci_key)
             tradenow, cci_trade, ccibb_trade = False, False, False
 
-    def define_times(self):
+    def define_times(self,ib):
+        # This whole block is trying to deal with the time differences between the server and TWS gateway.
+        # we have noticed a 1 second drift and it impacts our data calls
+        log.info("TWS time is: {tws} ".format(tws=self.ib.reqCurrentTime()))
+        log.info("PTH time is: {st}".format(st=datetime.now()))
+        #log.info("ib time min:{min}".format(min=self.ib.reqCurrentTime().minute))
+        #log.info("ib time second:{min}".format(min=self.ib.reqCurrentTime().second))
+        #
+        localDateTime = datetime.now()
+        twsTime = self.ib.reqCurrentTime()
+        twsTime = twsTime.replace(tzinfo=None)
+        #twsTime = twsTime.astimezone(timezone('America/New_York'))
+        twsTimeLocal = twsTime - timedelta(hours=5)
+        log.info("tws time in local time zone format: {t} ".format(t=twsTimeLocal))
+        twsDiff = localDateTime - twsTimeLocal 
+        log.info("tws to server time diff:{diff} ".format(diff=twsDiff))
+        newWait = localDateTime + timedelta(seconds=twsDiff)
+        log.info("additional delay in wait time added says wait time should be:{w}".format(w=newWait))
+#        if twsDiff.hour < 5:
+        #f localDateTime.second < twsDiff.second:
+        #    secondDiff = (locatDateTime.second + 60) - twsDiff.second
+        ###    print("second diff ",secondDiff)
+         #   minuteDiff = (locatDateTime.minute - 1) - twsDiff.minute
+         #   print("minuteDiff ",minuteDiff)
+        #else:
+        #    secondDiff = locatDateTime.second - twsDiff.second
+        #    print("second diff ",secondDiff)
+        #    minuteDiff = locatDateTime.minute - twsDiff.minute
+        #    print("minuteDiff ",minuteDiff)
+        #current_time + timedelta(minute=minuteDiff,second=secondDiff)
         if self.backTest:   # added for backtest
             current_time = self.backTestStartDateTime
             current_minute = self.backTestStartDateTime.minute
             self.backTestStartDateTime = current_time + timedelta(minutes=15)
             print("current time, backteststartdatetime",current_time,self.backTestStartDateTime)
         else:    
-            current_time = datetime.now()
+            current_time = datetime.now() + timedelta(seconds=5) #manually trying to augment time differences
             current_minute = datetime.now().minute
         log.info("currnet time: {ct} ".format(ct=current_time))
         if current_minute < 15:
             self.datetime_1h = current_time - timedelta(hours=1)
-            wait_time = current_time.replace(minute = 15,second=0) 
-            self.datetime_15 = current_time.replace(minute = 30, second = 0)
+            wait_time = current_time.replace(minute = 15,second=0, microsecond=0) 
+            self.datetime_15 = current_time.replace(minute = 30, second = 0, microsecond=0)
         elif current_minute < 30:
-            wait_time = current_time.replace(minute = 30,second=0) 
-            self.datetime_15 = current_time.replace(minute = 45, second=0)
+            wait_time = current_time.replace(minute = 30,second=0, microsecond=0) 
+            self.datetime_15 = current_time.replace(minute = 45, second=0, microsecond=0)
         elif current_minute < 45:
-            wait_time = current_time.replace(minute = 45,second=0) 
+            wait_time = current_time.replace(minute = 45,second=0, microsecond=0) 
             self.datetime_15 = current_time + timedelta(minutes=(45-current_minute+15))
-            self.datetime_15 =self.datetime_15.replace(second=0)
+            self.datetime_15 =self.datetime_15.replace(second=0, microsecond=0)
         else:
             wait_time = current_time + timedelta(minutes=(60-current_minute))
-            wait_time = wait_time.replace(second=0)
+            wait_time = wait_time.replace(second=0, microsecond=0)
             self.datetime_15 = current_time + timedelta(minutes=(60-current_minute+15))
-            self.datetime_15 =self.datetime_15.replace(second=0)
+            self.datetime_15 =self.datetime_15.replace(second=0, microsecond=0)
         if self.backTest:    #added for backtest
             wait_time = datetime.now() + timedelta(seconds=3)
             self.log_time = self.backTestStartDateTime
@@ -156,7 +186,7 @@ class Algo():
         #self.datetime_1h = self.log_time - timedelta(hours=1)
         self.datetime_1h = self.datetime_1h.replace(minute=0, second=0, microsecond=0)
         self.datetime_1d = current_time -  timedelta(days = 1)
-        self.datetime_1d =self.datetime_1d.replace(hour = 0, minute=0, second=0)
+        self.datetime_1d =self.datetime_1d.replace(hour = 0, minute=0, second=0, microsecond=0)
         log.info("log time: {lt} wait time: {wt} 1 hour: {one} day: {day}".format(lt = self.log_time,wt=wait_time,one=self.datetime_1h,day=self.datetime_1d))
         return wait_time,self.datetime_15,self.datetime_1h,self.datetime_1d,self.log_time
 
@@ -245,7 +275,7 @@ class Algo():
         tradeContract = self.ib.qualifyContracts(contContract)[0]   # gives all the details of a contract so we can trade it
         open_long, open_short, long_position_qty, short_position_qty = orders.countOpenPositions(self.ib)   # do we have an open position - not orders but positions?
         open_today = helpers.is_open_today(contracthours)
-        wait_time,self.datetime_15,self.datetime_1h,self.datetime_1d, self.log_time = self.define_times()
+        wait_time,self.datetime_15,self.datetime_1h,self.datetime_1d, self.log_time = self.define_times(self.ib)
         dataContract = Contract(exchange=config.EXCHANGE, secType="FUT", localSymbol=contContract.localSymbol)
         bars_15m = calculations.Calculations(self.ib, dataContract, "2 D", "15 mins", self.datetime_15)
         #print("bars15 cci_third, ccia_third, cci_prior, ccia_prior, cci, ccia",bars_15m.cci_third,bars_15m.ccia_third,bars_15m.cci_prior, bars_15m.ccia_prior, bars_15m.cci, bars_15m.ccia)
