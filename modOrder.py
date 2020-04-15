15eead39eed92af365d412b32b3add1a9d08d065
rkkreisel <randy.kreisel@gmail.com>
2020-04-15 11:44:58 -0400

    sync

--- modOrder.py
+++ modOrder.py
@@ -1,4 +1,5 @@
 from ib_insync import IB
+from ib_insync.order import Order
 from ib_insync.contract import ContFuture, Future
 from ib_insync.objects import BarDataList
 import datetime
@@ -14,7 +15,7 @@ from indicator import Indicator
 ib = IB()
 ib.connect(config.HOST, config.PORT, clientId=config.CLIENTID)
 
-buyPrice = '2720.00'
+buyPrice = '2770.75'
 
 contContract = ib.reqContractDetails(ContFuture(symbol=config.SYMBOL, exchange=config.EXCHANGE))
 tradeContract = contContract[0].contract
@@ -26,6 +27,9 @@ print("")
 openOrdersList = ib.openOrders()
 #print("contract",contract)
 print("open orders",openOrdersList)
+print("open orders",ib.openTrades())
+print("open trades ",ib.trades())
+print("orders ",ib.orders())
 print("length of orders ",len(openOrdersList))
 x = 0
 # not sure we need to differentiate between buy or sell stop orders below
@@ -42,8 +46,8 @@ while x < len(openOrdersList):
     #orderId, orderType, action, quantity = orders.parseOrderString(ib,openOrdersList)      
     #checkOrderStatus = updateCanceledOpenOrders(ib, orderId, trademkt)   # update each order that we cancelled
     print("action ",openOrdersList[x].action)
-    if openOrdersList[x].action == "SELL" and openOrdersList[x].orderType == "STP":
-        openOrdersList[x].auxPrice = 2777
+    if openOrdersList[x].action == "BUY" and openOrdersList[x].orderType == "STP":
+        openOrdersList[x].auxPrice = 2750.75
         print("new auxPrice",openOrdersList[x].auxPrice)
         openOrder = openOrdersList[x]
         print("type openOrder ",type(openOrder))
